9fbd7f88953ed912847c579de3942dfd9592b162
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-25 11:05:24 -0400

    Add files via upload

---
+++ filtering_with_each.rb
@@ -0,0 +1,10 @@
+fives = [5, 10, 15, 20, 25, 30, 35, 40]
+#I want to iterate over this array 
+
+fives.each do |value| 
+  if value.even?  
+    puts value #if current value is even, output value 
+  end 
+end 
+
+#we have iternated over every element 
