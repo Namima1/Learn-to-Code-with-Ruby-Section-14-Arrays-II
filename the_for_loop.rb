38073fe1a8bdbc0bc27ff6c80a6107a26ac64f36
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-25 13:22:43 -0400

    Add files via upload

---
+++ the_for_loop.rb
@@ -0,0 +1,9 @@
+# for element in iterable data structure 
+
+for number in [1, 2, 3]
+  puts number 
+end 
+
+#called a for loop bc we specify operation to run for each element in that data structure
+
+puts number 
