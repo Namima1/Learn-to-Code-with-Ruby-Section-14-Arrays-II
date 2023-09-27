5a71b47440d33b8bc1e03a643210bb743f801983
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-27 10:43:07 -0400

    Add files via upload

--- iteration_with_array_with_while_or_until_loops.rb
+++ iteration_with_array_with_while_or_until_loops.rb
@@ -6,3 +6,15 @@ while i < animals.length
   puts "The index is #{i} and the animal is #{animals[i]}"
   i += 1 
 end
+
+#i creates an infinite loop 
+# += means it raises by 1 
+
+
+i = 0 
+
+until i == animals.length 
+  puts "The index is #{i} and the animal is #{animals[i]}"
+  i += 1 
+end
+#until runs a loop as well 
