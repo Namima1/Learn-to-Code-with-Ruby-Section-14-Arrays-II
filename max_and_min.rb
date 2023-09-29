a8b46ebf8961a922b1e85e026f9038a6abf42f8e
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-29 11:45:37 -0400

    Add files via upload

---
+++ max_and_min.rb
@@ -0,0 +1,15 @@
+# max- greatest value in array 
+# min- smallest value in an array 
+
+stock_prices = [434.12, 723.99, 84.12, 649.92]
+
+p stock_prices.max
+p stock_prices.min 
+
+#if using max and min with words
+#ruby takes the max and min depending on the first letter of the word
+#and how far they are on the alphabet 
+
+companies = ["Nike", "Microsoft", "Apple"]
+p companies.max 
+p companies.min
